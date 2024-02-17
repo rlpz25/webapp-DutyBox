e17fcbc2c2ff46478e1d5ee8b09c752170b32047
rlpz25 <rlpz25@outlook.com>
2024-02-17 10:11:03 -0600

    DutyBox v1.0

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -9,75 +9,61 @@ class DashboardController extends Controller
 {
     public function dashboard()
     {
-        date_default_timezone_set('America/Mexico_City');
-        $current_date = date('Y-m-d');
-        $current_time = date('H:i:00');
-        $now = $current_date.' '.$current_time;
         $id = (string)auth()->user()->id;
-        // $indexes = 10;
         $duties = Duty::where('user_id', $id)->orderBy('updated_at','desc')->get();
-        // $pd_duties = Duty::whereRaw("user_id = $id AND duties.st_date < '$now' AND exp_date > '$now' AND duties.completed IS NULL ORDER BY exp_date ASC")->get();
-        // $ft_duties = Duty::whereRaw("user_id = $id AND completed  NULL AND st_date > '$now' ORDER BY st_date ASC")->get();
-        // $pd_duties = Duty::where('user_id', $id)->where('completed',NULL)->where('st_date','<',$now)->where('exp_date','>',$now)->orderBy('updated_at','desc')->get();
-        // $ft_duties = Duty::where('user_id', $id)->where('completed',NULL)->where('st_date','>',$now)->orderBy('st_date','asc')->get();
-        // $xp_duties = Duty::where('user_id', $id)->where('completed',NULL)->where('exp_date','<',$now)->orderBy('exp_date','desc')->get();
-        // $cp_duties = Duty::where('user_id', $id)->where('completed','!=',NULL)->orderBy('completed','desc')->get();
-        // $pd_duties = $pd_duties->fresh();
-        // $ft_duties = $ft_duties->fresh();
-        // $xp_duties = $xp_duties->fresh();
-        // $cp_duties = $cp_duties->fresh();
-        
-        // return view('dashboard',compact('pd_duties','cp_duties','ft_duties','xp_duties','current_date','current_time'));
-        return view('dashboard',compact('duties','current_date','current_time'));
+        return view('dashboard',compact('duties'));
     }
 
     public function upload(Request $request)
     {   
-        $duty = 0;
+        $duty = 'Data is missing:';
         if($request -> remove){
-            // $duty = 'es remove';
             if($request -> remove != '0'){
-                // $duty = 'tiene id el remove';
                 $duty = Duty::where('id',$request->remove)->delete();
+
+            } else {
+                $duty = $duty." id on remove";
+
             }
         } elseif ($request -> complete){
-            // $duty = 'es request';
             if($request -> complete != '0'){
-                // $duty = 'tiene id el request';
                 date_default_timezone_set('America/Mexico_City');
                 $duty = Duty::where('id',$request->complete)->first();
+
                 if($duty->completed == NULL){
                     $duty -> completed = date('Y-m-d H:i:00');
+
                 }else{
                     $duty -> completed = NULL;
+                
                 }
                 $duty -> save();
+            
+            } else {
+                $duty = $duty." id on complete";
+            
             }
-        }
-        
-        if($request -> name && $request -> exp_date && $request -> st_date){
-            $request -> validate([
-                'name' => 'required',
-                'exp_date' => 'required',
-                'st_date' => 'required',
-            ]);
+        } else if($request -> name && $request -> exp_date && $request -> st_date){
             if ($request -> modify){
                 if($request -> modify != '0'){
                     $duty = Duty::where('id',$request->modify)->first();
+
                 }
             }else{
                 $duty = new Duty();
-            }
-            $duty -> name = $request -> name;
-            $duty -> description = $request -> description;
-            $duty -> st_date = $request->st_date." ".$request->st_time;
-            $duty -> exp_date = $request->exp_date." ".$request->exp_time;  
-            $duty -> user_id = auth()->user()->id;
-            $duty -> save();
-        } else {
-            $duty = 'Required data';
-        }   
 
+            }
+                $duty -> name = $request -> name;
+                $duty -> description = $request -> description;
+                $duty -> st_date = $request->st_date." ".$request->st_time;
+                $duty -> exp_date = $request->exp_date." ".$request->exp_time;  
+                $duty -> user_id = auth()->user()->id;
+                $duty -> save();
+
+        } else {
+            $duty = $duty.' name, start date or expiration date on upsert';
+
+        }
         return $duty;
     }
 }
